d665a6add756c086e87e4506b15c7231bd0c3bd3
jartymannetty <41650367+jartymannetty@users.noreply.github.com>
2018-07-26 09:13:56 -0500

    Add project files.

---
+++ HelloName.Test/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace HelloName.Test
+    {
+        public class AppInitializer
+        {
+            public static IApp StartApp(Platform platform)
+            {
+                if (platform == Platform.Android)
+                {
+                    return ConfigureApp
+                           .Android
+                           .EnableLocalScreenshots()
+                           .ApkFile(@"../../../HelloName/HelloName.Android/bin/Debug/com.companyname.HelloName.apk")
+                           .StartApp();
+                }
+
+                return ConfigureApp
+                       .iOS
+                       .EnableLocalScreenshots()
+                       .StartApp();
+            }
+        }
+    }
